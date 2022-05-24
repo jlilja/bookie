5913bffadbdf89d5e979c6de6828f8cd3cd9d0df
Jonas Lilja <jonas@lilja.io>
2022-05-24 22:36:00 +0200

    Added more comments on how to find your profile and steps on how to clear the lock file.

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ import time
 # Here's a page about the lockfile. It needs to be removed before starting FF next time.
 # Its probably why I have sooo much inconsistencies when developing.
 
+# I found it. Its the file conveniently named 'lock'.
+# So, when you run this script. Make sure to close down FF, delete the lock file, run this file, verify in a sql client.
+# If you are lucky then the new bookmark(s) should be populated.
+# Probably need to add a step to remove the lock file and check if there's an open FF session?
+
 # table moz_bookmarks
 # 	- id = auto incremental id
 # 	- type = type 1 is bookmark, type 2 is folder
@@ -25,13 +30,20 @@ def findFirefoxDatabase():
 	file = 'places.sqlite'
 
 	dirs = os.listdir(basePath)
-	match = list(filter(lambda dir: dir.endswith(wantedSuffix), dirs))
+	getProfileMatches = lambda dir: dir.endswith('.default') or dir.endswith('.default-release')
+	matches = list(filter(getProfileMatches, dirs))
 
-	sqlitePath = '/'.join([basePath, match[0], file])
+	# Unsure what profile your firefox is using? Browse to about:profiles and it'll tell you.
+	# TODO: Either parse the current profile, or add a drop down menu to select which one you want.
+	print(f'Found {len(matches)} profile(s). Which one do you want? {matches}')
+
+	myTemporaryProfile = 'zocz1232.default-release'
+
+	sqlitePath = '/'.join([basePath, myTemporaryProfile, file])
 
 	if os.path.isfile(sqlitePath):
 		print(f'Found sqlite file at {sqlitePath}')
-		
+
 		return sqlitePath
 
 	print(f'Could not find Firefox sqlite file at {sqlitePath}')
@@ -39,8 +51,8 @@ def findFirefoxDatabase():
 if __name__ == '__main__':
 	tmpdir = tempfile.gettempdir()
 	# shutil.copy(os.path.join('/home/j/.mozilla/firefox/37rfrt13.default', "places.sqlite"), tmpdir)
-	print(findFirefoxDatabase())
-	conn = sqlite3.connect('places.sqlite')
+	pathToFile = findFirefoxDatabase()
+	conn = sqlite3.connect(pathToFile)
 
 	cursor = conn.cursor()
 
@@ -57,15 +69,13 @@ if __name__ == '__main__':
 	cursor.execute( """INSERT INTO moz_bookmarks
                           (type, fk, parent, position, title, dateAdded) 
                            VALUES 
-                          (1, 2707, 45, 5, 'och nu15', 1636148302936001)""")
+                          (1, 16, 11, 5, 'och nu15', 1636148302936001)""")
 	conn.commit()
 
-	# cursor.execute("""SELECT title FROM moz_bookmarks;""")
-	# rows2 = cursor.fetchall()
+	cursor.execute("""SELECT title FROM moz_bookmarks;""")
+	rows2 = cursor.fetchall()
 
-
-
-	# print(rows2)
+	print(rows2)
 
 	conn.close()
 
